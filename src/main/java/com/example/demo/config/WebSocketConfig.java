9997ceaffdd973fc00ada4182af40bc00fe43501
Elena Lazareva <elena.lazareva@hva.nl>
2024-12-01 17:25:23 +0100

    Unnecessary classes deleted

--- src/main/java/com/example/demo/config/WebSocketConfig.java
+++ src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -3,8 +3,6 @@ package com.example.demo.config;
 import com.example.demo.security.WebsocketChannelInterceptor;
 import com.example.demo.service.JwtService;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -19,7 +17,7 @@ import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
-@Order(Ordered.HIGHEST_PRECEDENCE + 99)
+//@Order(Ordered.HIGHEST_PRECEDENCE + 99)
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     private final WebsocketChannelInterceptor interceptor;
     private final JwtService jwtService;
@@ -51,10 +49,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                             try {
                                 // Use JwtService to validate and extract the username
                                 String username = jwtService.extractUsername(token);
+                                System.out.println("Username extracted from token: " + username);
 
                                 if (username != null) {
+                                    Principal principal = () -> username;
+                                    attributes.put("principal", principal);
                                     System.out.println("Assigned Principal: " + username);
-                                    return () -> username; // Return a Principal with the username
+                                    return principal; // Return a Principal with the username
                                 }
                             } catch (Exception e) {
                                 System.out.println("Token validation failed: " + e.getMessage());
